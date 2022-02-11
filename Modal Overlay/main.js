ddd40aae6b7000cdae561ff517fc27f795608ef1
Sahil Maheshwari <sahilm1711@gmail.com>
2022-02-11 12:33:47 +0530

    Added Modal Overlay solution

---
+++ Modal Overlay/main.js
@@ -0,0 +1,11 @@
+let Modal=document.getElementById("modal")
+let Button=document.getElementById("Btn")
+Modal.style.display="none"
+function showModal(){
+    Modal.style.display="block"
+    Button.style.display="none"
+}
+function hideModal(){
+    Modal.style.display="none"
+    Button.style.display="block"
+}
