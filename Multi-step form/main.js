0f6ec029489b522cde1977b6dd979d85a91e8ae2
Sahil Maheshwari <sahilm1711@gmail.com>
2022-02-14 21:33:26 +0530

    Adding the form as the solution

--- Multi-step form/main.js
+++ Multi-step form/main.js
@@ -1,1 +1,57 @@
 let currentPage=0
+let NameForm=document.getElementById("NameForm")
+let EmailForm=document.getElementById("EmailForm")
+let DateForm=document.getElementById("DateForm")
+let PasswordForm=document.getElementById("PasswordForm")
+let SuccessForm=document.getElementById("SuccessForm")
+EmailForm.style.display="none"
+PasswordForm.style.display="none"
+DateForm.style.display="none"
+SuccessForm.style.display="none"
+function changePage(){
+     event.preventDefault()
+     currentPage++;
+     if(currentPage ===1){
+         NameForm.style.display="none"
+         EmailForm.style.display="block"
+     }else if(currentPage ===2){
+         EmailForm.style.display="none"
+         DateForm.style.display="block"
+     }else if (currentPage ===3){
+         DateForm.style.display="none"
+         PasswordForm.style.display="block"
+     }
+}
+
+function previousPage(){
+    event.preventDefault();
+    currentPage--;
+    if(currentPage === 0){
+        NameForm.style.display="block"
+        EmailForm.style.display="none"
+        DateForm.style.display="none"
+        PasswordForm.style.display="none"
+    }
+    else if(currentPage ===1){
+         NameForm.style.display="none"
+         EmailForm.style.display="block"
+         DateForm.style.display="none"
+         PasswordForm.style.display="none"
+     }else if(currentPage ===2){
+         EmailForm.style.display="none"
+         DateForm.style.display="block"
+         PasswordForm.style.display="none"
+     }else if (currentPage ===3){
+         DateForm.style.display="none"
+         PasswordForm.style.display="block"
+         EmailForm.style.display="none"
+     }
+}
+
+function showSuccess() {
+    event.preventDefault()
+    EmailForm.style.display="none"
+    PasswordForm.style.display="none"
+    DateForm.style.display="none"
+    SuccessForm.style.display="block"
+}
